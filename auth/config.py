beaf8f0d68fb9da35ea5e7ca82ec9ace7307df9d
ASIK SK <140266771+ASIK140@users.noreply.github.com>
2025-05-25 06:02:41 +0530

    Update config.py

--- auth/config.py
+++ auth/config.py
@@ -2,8 +2,8 @@
 SMTP_CONFIG = {
     "SERVER": "smtp.gmail.com",
     "PORT": 587,
-    "USERNAME": "asikeducation140@gmail.com",  # Replace with your Gmail
-    "PASSWORD": "ngah mjua upgl rfgj",  # Replace with app password
-    "FROM": "asikeducation140@gmail.com",  # Should match USERNAME
+    "USERNAME": "",  # Replace with your Gmail
+    "PASSWORD": "",  # Replace with app password
+    "FROM": "",  # Should match USERNAME
     "TIMEOUT": 10  # seconds
 }
